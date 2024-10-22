93871b89a251afa56a4c6ef9196677330ea0ff74
FrozenWay86 <91466247+FrozenWay86@users.noreply.github.com>
2024-10-22 12:52:33 +0200

    Update yt links

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const videoContainer = document.getElementById('video-container');
 
 // Liste des vidéos YouTube
 const videos = [
-    'https://youtu.be/3P14G2Che-8?si=vxDYyeSYu_llFf1U', // Remplace VIDEO_ID_1 par l'ID de ta première vidéo
-    'https://youtu.be/T7d7ULdGyP4?si=Xj8RxXNPcGEZ8vwJ', // Remplace VIDEO_ID_2 par l'ID de ta deuxième vidéo
-    'https://youtu.be/UVp27NR5R6Q?si=uOpK9HHQFS0a1L6V'  // Remplace VIDEO_ID_3 par l'ID de ta troisième vidéo
+    'https://www.youtube.com/watch?v=3P14G2Che-8', // Remplace VIDEO_ID_1 par l'ID de ta première vidéo
+    'https://www.youtube.com/watch?v=UVp27NR5R6Q', // Remplace VIDEO_ID_2 par l'ID de ta deuxième vidéo
+    'https://www.youtube.com/watch?v=IbINR6TNhFw'  // Remplace VIDEO_ID_3 par l'ID de ta troisième vidéo
 ];
 
 // Fonction pour charger les vidéos
