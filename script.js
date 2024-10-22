f442eadecad4c3efd5b8a106e5689b8f65db14c8
FrozenWay86 <91466247+FrozenWay86@users.noreply.github.com>
2024-10-22 12:56:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const videoContainer = document.getElementById('video-container');
 
-// Liste des vidéos YouTube
+// Liste des vidéos YouTube avec URLs d'intégration
 const videos = [
-    'https://www.youtube.com/watch?v=3P14G2Che-8', // Remplace VIDEO_ID_1 par l'ID de ta première vidéo
-    'https://www.youtube.com/watch?v=UVp27NR5R6Q', // Remplace VIDEO_ID_2 par l'ID de ta deuxième vidéo
-    'https://www.youtube.com/watch?v=IbINR6TNhFw'  // Remplace VIDEO_ID_3 par l'ID de ta troisième vidéo
+    'https://www.youtube.com/embed/3P14G2Che-8',
+    'https://www.youtube.com/embed/T7d7ULdGyP4',
+    'https://www.youtube.com/embed/UVp27NR5R6Q'
 ];
 
 // Fonction pour charger les vidéos
